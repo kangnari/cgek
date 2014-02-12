b970d5d7893427ee8f08b5d14b50e45b872db2e8
Eugenia Kang <enarikang@gmail.com>
2014-02-12 00:53:54 -0800

    Full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) { "CGEK" }
 	describe "Home page" do
 		it "should have content 'CGEK' " do
 			visit root_path
 			expect(page).to have_content('CGEK')
+		end
+
+		it "should have the base title" do
+			visit root_path
+			expect(page).to have_title("CGEK")
+		end
+
+		it "should not have a custom page title" do
+			visit root_path
+			expect(page).not_to have_title('Home - ')
 		end
 	end
 end
