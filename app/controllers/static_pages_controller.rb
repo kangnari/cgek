dbfbd2e2c7db132321ed76b8de3184bb290b4aa0
Eugenia Kang <enarikang@gmail.com>
2014-03-05 02:56:06 -0800

    Layout changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 	def home
 		@post = current_user.posts.build if signed_in?
-		@posts = Post.paginate(page: params[:page])
+		@posts = Post.paginate(page: params[:page], :per_page => 20)
 	end
 
 	def resources
